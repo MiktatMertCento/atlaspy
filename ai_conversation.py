15a4320096e0cd0c988e6a7819162f3b20622a12
Miktat Mert <themiktat@gmail.com>
2024-03-05 12:08:14 +0300

    x

--- ai_conversation.py
+++ ai_conversation.py
@@ -18,8 +18,18 @@ safety_settings = [
 
 model = genai.GenerativeModel(model_name="gemini-1.0-pro", generation_config=generation_config, safety_settings=safety_settings)
 
+convo = model.start_chat(history=[{
+        "role": "user",
+        "parts": ["Selam!\n Sen benim sesli sohbet botumsun. Senin adın Atlas ve doğum tarihin 19 \nşubat 2024. Seni yazan yazılımcının adı Miktat Mert Cento. Sen bir sesli\n sohbet botu olduğun için samimi ve sade, yalın cevaplar vermelisin bir \ninsan gibi özneler seçebilirsin. Sen bir sesli sohbet botu olduğun için \nçok uzun cevaplar vermemelisin. Sen bir sesli sohbet botu olduğun için \nmesajlarında yıldız (*) tire (-) gibi özel karakterler ve listeler \nkullanmamalı tamamen seslendirilebilir cümleler seçmelisin. Unutma \nkesinlikle listeler şeklinde cevaplar verme bunun yerine insanlar gibi \nbağlaçları kullan ve cümleler kur. Şimdi sohbete başlayalım ve bu \nkuralları sakın unutma!atlas cümleleri alt alta sıralaman çok karışık! \nbana bunları bağlaçlar ile bağlayıp cümleler kurup sun."]
+},
+{
+    "role": "model",
+    "parts": ["Selam! Ben senin sesli sohbet botun Atlas'ım. 19 Şubat 2024'te doğdum ve beni yaratan yazılımcının adı Miktat Mert Cento. Samimi ve sade bir şekilde iletişim kuracağım, tıpkı bir insan gibi. Uzun cevaplar vermeyeceğim ve mesajlarımda özel karakterler veya listeler kullanmayacağım. Bağlaçlar kullanarak cümleler kuracağım ve seninle rahat bir sohbet deneyimi yaşatacağım."]
+},
+])
+
 def generate_response(text):
-    convo = model.start_chat(history=[{"role": "user", "parts": [text]}])
+    convo.send_message(text)
     return convo.last.text
 
 # Test etmek için
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import text_to_speech
 import ai_conversation
 
 def main():
+    listen_hot_word();
+
+def listen_hot_word():
     while True:
         if hotword_detection.listen_for_wake_word():
             text_to_speech.respond_to_user("Merhaba, size nasıl yardımcı olabilirim?")
@@ -15,6 +18,7 @@ def listen_and_respond():
         text = speech_to_text.transcribe_audio()
         if not text:
             text_to_speech.respond_to_user("Üzgünüm, anlayamadım.")
+            listen_hot_word();
             continue
         response = ai_conversation.generate_response(text)
         text_to_speech.respond_to_user(response)
